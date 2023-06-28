f0acc82a199f5122eccc9563511fa4f0fa031744
Zewei Tao <zeweitao@630.com>
2023-06-28 15:12:02 +0800

    i

--- cpp_programming_guide/cpp_launguage_extention/warp_shuffle_functions/broadcast_in_warp.cu
+++ cpp_programming_guide/cpp_launguage_extention/warp_shuffle_functions/broadcast_in_warp.cu
@@ -6,12 +6,14 @@ void
 broad_cast_from_zero_thread_in_warp(
     int arg)
 {
-    int laneId = threadIdx.x;
-    int value;
-    if(laneId == 0){
+    int laneId = threadIdx.x & 0x1f; // landId是线程在一个warp中的id，一个warp有32个线程，所以与上0x1f
+    int value = 1;
+    if(laneId % 8 == 0){
         value = arg;
     }
-    value = __shfl_sync(0xffffffff, value, 0);
+    // 注意到，第四个参数是width，相当于把一个warp继续分割，然后每一个subsection的id都是从0开始
+    // 如果是8的话，那么就是0,1,2,3,4,5,6,7\0,1,2,3,4,5,6,7\0,1,2,3,4,5,6,7\0,1,2,3,4,5,6,7这样一个区间
+    value = __shfl_sync(0xffffffff, value, 0, 8); 
     if (value != arg){
         printf("Thread %d failed.\n", threadIdx.x);
     }   
